a48999b4c17931fe7319879e99dfbdd505fc6941
21173447 <tshegofatsononyane009@gmail.com>
2024-08-19 00:10:29 +0200

    .

--- Enter-Stream-master/src/Components/AllMovies.jsx
+++ Enter-Stream-master/src/Components/AllMovies.jsx
@@ -63,7 +63,7 @@ const AllMovies = () => {
           </div>
         )}
       </div>
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-0 py-40">
+      <div className="grid grid-cols-2 sm:grid-cols-3 md:grid-cols-4 gap-0 py-40">
         {movies.length === 0 ? (
           <p>No movies available</p>
         ) : (
